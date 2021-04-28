b2ee26bc77835462a3e607793c5a60454739366f
Bizblk <enemy_counter@mail.ru>
2021-04-28 08:24:38 +0300

    add description

--- AnimationHW/ViewController.swift
+++ AnimationHW/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var animationView: SpringView!
     @IBOutlet weak var animationButton: SpringButton!
+    @IBOutlet weak var animationDescription: UILabel!
     
     private var index = 0
     private var animations = Animation.getAnimations()
@@ -36,7 +37,10 @@ class ViewController: UIViewController {
         animationView.curve = animations[index].curve.rawValue
         animationView.force = animations[index].force
         animationView.delay = animations[index].delay
+        animationView.duration = animations[index].duration
         animationView.animate()
+        getAnimationDescription()
+
         
         
         if index != animations.count {
@@ -45,6 +49,16 @@ class ViewController: UIViewController {
             index = 0
         }
         sender.setTitle(animations[index].preset.rawValue, for: .normal)
+    }
+    
+    private func getAnimationDescription() {
+        animationDescription.text = """
+            preset: \(animations[index].preset.rawValue)
+            curve: \(animations[index].curve.rawValue)
+            force: \(animations[index].force)
+            delay: \(animations[index].delay)
+            duration: \(animations[index].duration)
+            """
     }
     
     
